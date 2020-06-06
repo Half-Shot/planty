7bfec26cc012efc2a7c84d773955aa6ec4d67cff
Will Hunt <will@half-shot.uk>
2020-06-06 17:49:51 +0100

    Add various bits of logic

--- listener/index.js
+++ listener/index.js
@@ -2,7 +2,7 @@ const dgram = require('dgram');
 const os = require('os');
 
 const PORT = 5000;
-const HOST = getLocalAddress();
+const HOST = process.env.PL_LISTEN_ADDRESS || getLocalAddress();
 
 function getLocalAddress() {
     const iface = Object.values(
@@ -23,8 +23,21 @@ server.on('listening', () => {
     console.log(`UDP Server listening on ${address.address}:${address.port}`);
 });
 
+let lastScrapeTime = Date.now();
 server.on('message', (message, remote) => {
-    console.log(`${remote.address}:${remote.port} - ${message}`);
+    const now = Date.now();
+    console.log(message);
+    const [checksum, temp, moist, photo] = message;
+    const wifiAttempts = (message[4] >> 8) + message[5];
+    const txnNumber = (message[6] >> 24) + (message[7] >> 16) + (message[8] >> 8) + message[9];
+    const expectedCSum = ((temp*5) + (moist*2) + (photo*3) + wifiAttempts + txnNumber) % 255;
+    if (checksum !== expectedCSum) {
+        console.warn(`Incorrect checksum: expected ${expectedCSum}, got ${checksum}`);
+        return;
+    }
+    const inteval = now - lastScrapeTime;
+    lastScrapeTime = now;
+    console.log(`${new Date().toString()} ${remote.address}:${remote.port} - txn: ${txnNumber} temp: ${temp}, moist: ${moist}, photo: ${photo} watmpt: ${wifiAttempts} (interval: ${inteval}ms)`);
 });
 
-server.bind(PORT, HOST);
+server.bind(PORT, HOST);        
