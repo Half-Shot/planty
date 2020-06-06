9bb254ecda59da4912ec35bfc053d26574417657
Will Hunt <will@half-shot.uk>
2020-06-06 17:49:31 +0100

    Finish off low power code and a whole bunch of things

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -1,150 +1,132 @@
 /*
+  This application sends a UDP packet to a given host every
+  interval, containing data from different sensors.
 
-  This example connects to a encrypted WiFi network (WPA/WPA2).
-  Then it prints the  MAC address of the board,
-  the IP address obtained, and other network details.
-  Then it continuously pings given host specified by IP Address or name.
-
-  Circuit:
-  * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
-
-  created 13 July 2010
-  by dlf (Metodo2 srl)
-  modified 09 June 2016
-  by Petar Georgiev
+  Written by Half-Shot <will@half-shot.uk> 2020
 */
+
+#define LOWPOWER 1
+/* #define SERIALDEBUG 1 */
+
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
-
 #include "arduino_secrets.h" 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+
+#ifdef LOWPOWER
+#include "ArduinoLowPower.h"
+#endif
+
+
+
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;     // the WiFi radio's status
+IPAddress hostIp (192, 168, 0, 48);
+unsigned int hostPort = 5000;
+int sleepInterval = 1000 * 60 * 15; // 5 minutes
 
-// Specify IP address or hostname
-char hostName[] = "192.168.0.30";
-int hostPort = 5000;
-
-byte buffer[4]; // one byte for checksum, one byte for temperature, one byte for moisutre, one byte for photo 
+int status = WL_DISCONNECTED;     // the WiFi radio's status
+byte buffer[10]; // one byte for checksum, one byte for temperature, one byte for moisutre, one byte for photo, two for connection attempts, 4 for txn number
+uint16_t wifiAttempts = 0;
+uint32_t txnNumber = 0;
 WiFiUDP Udp;
+
 void setup() {
   // Initialize serial and wait for port to open:
+  #ifdef SERIALDEBUG
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  #endif
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
+    #ifdef SERIALDEBUG
     Serial.println("Communication with WiFi module failed!");
+    #endif
     // don't continue
     while (true);
   }
 
   String fv = WiFi.firmwareVersion();
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    #ifdef SERIALDEBUG
     Serial.println("Please upgrade the firmware");
+    #endif
   }
+}
 
-  // attempt to connect to WiFi network:
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 5 seconds for connection:
+bool connectWifi() {
+  // Connect to WPA/WPA2 network:
+  while (status != WL_CONNECTED) {
+    if (status == WL_IDLE_STATUS) {
+      status = WiFi.status();
+    } else {
+      status = WiFi.begin(ssid, pass);
+    }
+    wifiAttempts++;
+    if (wifiAttempts % 8 == 0) {
+      // Power down to save battery life. Try again on the next cycle. 
+      return false;
+    }
+    // wait 4 seconds for connection:
     delay(5000);
   }
+  wifiAttempts = 0;
+  return true;
+}
 
-  // you're connected now, so print out the data:
-  Serial.println("You're connected to the network");
-  printCurrentNet();
-  printWiFiData();
+void buildBuffer() {
+    buffer[1] = 10; // temp
+    buffer[2] = 32; // moist
+    buffer[3] = 150; // photo
+
+    // Wifi attempts
+    buffer[4] = wifiAttempts >> 8; 
+    buffer[5] = wifiAttempts % 255;
+
+    // Txn number
+    buffer[6] = (txnNumber & 0xff000000) >> 24;
+    buffer[7] = (txnNumber & 0x00ff0000) >> 16;
+    buffer[8] = (txnNumber & 0x0000ff00) >> 8;
+    buffer[9] = (txnNumber & 0x000000ff);
+
+    // Checksum
+    buffer[0] = ((buffer[1]*5) + (buffer[2]*2) + (buffer[3]*3) + wifiAttempts + txnNumber) % 255;
 }
 
 void loop() {
-  int result = -1;
+  // Bring up interface
+  if (connectWifi()) {
+    Udp.begin(5001);
+    int result = -1;
+    buildBuffer();
 
-  buffer[1] = 10;// temp
-  buffer[2] = 32;// moist
-  buffer[3] = 150;// photo
-  Serial.println("wrote bytes");
-  int checksum = ((buffer[1]*5) + (buffer[2]*2) + (buffer[3]*3)) / 16;
-  Serial.print("Checksum:");
-  buffer[0] = checksum;
-  Serial.println(checksum);
-  result = Udp.beginPacket(hostName, hostPort);
-  if (result != 1) {
-    Serial.println("Failed to reach host, retrying in 5000ms");
-    delay(5000);
-    return;
-  }
-  Serial.println("Udp.beginPacket");
-  Udp.write(buffer, 4);
-  Serial.println("Udp.write");
-  result = Udp.endPacket();
-  if (result != 1) {
-    Serial.println("Failed to send packet, retrying in 5000ms");
-    delay(5000);
-    return;
-  }
+    // Write packet
+    Udp.beginPacket(hostIp, hostPort);
+    Udp.write(buffer, 10);
+    Udp.flush();
+    result = Udp.endPacket();
+    txnNumber++;
 
-  delay(5000);
-}
-
-void printWiFiData() {
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP address : ");
-  Serial.println(ip);
-
-  Serial.print("Subnet mask: ");
-  Serial.println((IPAddress)WiFi.subnetMask());
-
-  Serial.print("Gateway IP : ");
-  Serial.println((IPAddress)WiFi.gatewayIP());
-
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI): ");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type: ");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printMacAddress(byte mac[]) {
-  for (int i = 5; i >= 0; i--) {
-    if (mac[i] < 16) {
-      Serial.print("0");
+    #ifdef SERIALDEBUG
+    if (result != 1) {
+      Serial.println("Failed to send packet");
     }
-    Serial.print(mac[i], HEX);
-    if (i > 0) {
-      Serial.print(":");
-    }
+    #endif
+
+    delay(1000);
+    Udp.stop();
   }
-  Serial.println();
+
+  // Stop interfaces
+  WiFi.end();
+  status = WL_DISCONNECTED;
+  // Sleep
+  #ifdef LOW_POWER
+  LowPower.sleep(sleepInterval);
+  #else
+  delay(sleepInterval);
+  #endif
 }
